6b064f55bfaddf879515b54840d1c0aa6e8316b0
leandro <leandro@matilha.digital>
2019-04-11 15:48:01 -0300

    estrutura backend

---
+++ src/models/Box.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+const Box = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    files: [
+        {
+            type: mongoose.Schema.Types.ObjectId, ref: "File"
+        }
+    ]
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model("Box", Box);
